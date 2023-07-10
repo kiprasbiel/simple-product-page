12dbc84a318bd0146631f8a0841091c6584b6b9a
Kipras Bielinskas <kipras.bielinskas@kantar.com>
2023-07-10 18:26:19 +0300

    Created Product Tag model with its migration for storing all related Product Tags. Added relations to both models.

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Product extends Model
@@ -12,5 +13,9 @@ class Product extends Model
 
     public function content(): HasOne {
         return $this->hasOne(ProductContent::class);
+    }
+
+    public function tags(): HasMany {
+        return $this->hasMany(ProductTag::class);
     }
 }
