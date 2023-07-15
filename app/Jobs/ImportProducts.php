7013f1911b0b0d77325049c4ad35eb535950a876
Kipras Bielinskas <kipras.bielinskas@kantar.com>
2023-07-15 13:00:03 +0300

    Added json file row validation to ImportProducts.php.

--- app/Jobs/ImportProducts.php
+++ app/Jobs/ImportProducts.php
@@ -3,12 +3,15 @@
 namespace App\Jobs;
 
 use App\Models\Product;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Validation\ValidationException;
 
 class ImportProducts implements ShouldQueue
 {
@@ -28,21 +31,43 @@ class ImportProducts implements ShouldQueue
         }
     }
 
+    /**
+     * @throws ValidationException
+     */
     private function execute(array $products): void {
         foreach ($products as $product) {
+            $validator = Validator::make($product, [
+                'sku' => 'required|string',
+                'size' => 'required|string',
+                'photo' => 'required|url',
+                'description' => 'required|string',
+                'updated_at' => 'date',
+                'tags'  => 'array',
+            ]);
+
+            if ($validator->fails()) {
+                Log::warning('Product importer row validator failed. Failed: ' .
+                    var_export($validator->failed(), true)
+                );
+                Log::warning('Failed row: ' . var_export($product, true));
+                continue;
+            }
+
+            $validated = $validator->validated();
+
             $newProduct = Product::create([
-                'SKU' => $product['sku'],
-                'size' => $product['size'],
-                'photo_url' => $product['photo'],
-                'updated_at' => $product['updated_at']
+                'SKU' => $validated['sku'],
+                'size' => $validated['size'],
+                'photo_url' => $validated['photo'],
+                'updated_at' => $validated['updated_at']
             ]);
 
             $newProduct->content()->create([
-                'description' => $product['description']
+                'description' => $validated['description']
             ]);
 
             $newProduct->tags()->createMany(
-                $product['tags']
+                $validated['tags']
             );
         }
     }
