0e7a3b3508096773d19627a69e4bce3f4b96ad64
Kipras Bielinskas <kipras.bielinskas@kantar.com>
2023-07-15 15:08:20 +0300

    Created new ProductStock model and its table migration.

---
+++ database/migrations/2023_07_15_115916_create_product_stocks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_stocks', function (Blueprint $table) {
+            $table->id();
+            $table->string('SKU');
+            $table->integer('stock');
+            $table->string('location');
+            $table->timestamps();
+
+            $table->foreign('SKU')
+                ->references('SKU')
+                ->on('products')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_stocks');
+    }
+};
