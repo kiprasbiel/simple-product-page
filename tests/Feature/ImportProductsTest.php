caa2b347cda5313c7c5d7a46c697d0a8a1cd096c
Kipras Bielinskas <kipras.bielinskas@kantar.com>
2023-07-15 14:41:31 +0300

    In ImportProductsTest now faking product import storage and at the start of test adding a test product import file. Updated expected asserted test_import_products values.

--- tests/Feature/ImportProductsTest.php
+++ tests/Feature/ImportProductsTest.php
@@ -4,11 +4,19 @@ namespace Tests\Feature;
 
 use App\Jobs\ImportProducts;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 
 class ImportProductsTest extends TestCase
 {
     use RefreshDatabase;
+
+    public function setUp(): void {
+        parent::setUp();
+        Storage::fake('product_import');
+        Storage::disk('product_import')->put('products-test.json', '[{"sku": "KF-004", "description": "Poisoning by hydantoin derivatives, self-harm, init", "size": "2XL", "photo": "http://dummyimage.com/125x100.png/cc0000/ffffff", "tags": [], "updated_at": "2022-04-29"}, {"sku": "KF-025", "description": "Displ transverse fx shaft of unsp tibia, 7thJ", "size": "M", "photo": "http://dummyimage.com/218x100.png/5fa2dd/ffffff", "tags": [], "updated_at": "2022-02-04"}, {"sku": "KF-041", "description": "Nonspecific urethritis", "size": "3XL", "photo": "http://dummyimage.com/130x100.png/5fa2dd/ffffff", "tags": [{"title": "Cookley"}], "updated_at": "2022-02-01"}]');
+    }
+
     /**
      * Testing if correct amount of DB records are created.
      */
@@ -16,8 +24,8 @@ class ImportProductsTest extends TestCase
     {
         (new ImportProducts())->handle();
 
-        $this->assertDatabaseCount('products', 97);
-        $this->assertDatabaseCount('product_contents', 97);
-        $this->assertDatabaseCount('product_tags', 48);
+        $this->assertDatabaseCount('products', 3);
+        $this->assertDatabaseCount('product_contents', 3);
+        $this->assertDatabaseCount('product_tags', 1);
     }
 }
