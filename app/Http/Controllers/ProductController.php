84912ede86812c67740a42f687cbf2a49c67768b
Kipras Bielinskas <kipras.bielinskas@kantar.com>
2023-07-15 19:04:26 +0300

    Created ProductController with simple single API route for fetching all products.

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Collection;
+
+class ProductController extends Controller
+{
+    public function index(): Collection{
+        return Product::with(['content', 'tags', 'stocks'])->get();
+    }
+}
