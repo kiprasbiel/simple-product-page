46b0cb451323ebb3c772b34d1baaee067568cdfa
Kipras Bielinskas <kipras.bielinskas@kantar.com>
2023-07-15 15:06:21 +0300

    Updated products table migration by making SKU unique.

--- database/migrations/2023_07_10_143929_create_products_table.php
+++ database/migrations/2023_07_10_143929_create_products_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('SKU');
+            $table->string('SKU')->unique();
             $table->string('size', 5);
             $table->string('photo_url');
             $table->timestamps();
