362187de621844b51a6a79c76a394162587c5967
Kipras Bielinskas <kipras.bielinskas@kantar.com>
2023-07-16 14:54:38 +0300

    Created new authentication controller for handling registration and login of users.

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+
+
+class AuthController extends Controller
+{
+    /**
+     * @throws ValidationException
+     */
+    public function register(Request $request): array {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+            'repeat_password' => 'required|same:password',
+        ]);
+
+        if($validator->fails()){
+            return ['message' => 'Registration failed.', 'error' => $validator->errors()];
+        }
+
+        $validated = $validator->validated();
+
+        $validated['password'] = Hash::make($validated['password']);
+        $user = User::create($validated);
+
+        return [
+            'message' => 'Registration successful.',
+            'token' => $user->createToken('MyApp')->plainTextToken
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/products', [ProductController::class, 'index']);
+
+Route::controller(AuthController::class)->group(function(){
+    Route::post('register', 'register');
+});
