eb03447cebb9e64e12fa0db21d3782eccaab22a4
Kipras Bielinskas <kipras.bielinskas@kantar.com>
2023-07-28 17:02:49 +0300

    Added new test for checking if single product API endpoint is working correctly.

--- tests/Feature/ApiTest.php
+++ tests/Feature/ApiTest.php
@@ -23,4 +23,17 @@ class ApiTest extends TestCase
 
         $response->assertJsonCount(10);
     }
+
+    public function test_api_get_single_product(): void {
+        Product::factory()->count(1)->create([
+            'SKU' => 'SKU-001'
+        ]);
+
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->get('/api/product/SKU-001');
+
+        $response->assertStatus(200);
+        $this->assertEquals('SKU-001', $response->original->SKU);
+    }
 }
