76f70426b9cc33d85e27cd6831f00ab6bb67738d
Kipras Bielinskas <kipras.bielinskas@kantar.com>
2023-07-31 15:40:05 +0300

    Instead of returning all available Product data from ProductController::index, using new ProductsResource returning only required data.

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,14 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\ProductsResource;
 use App\Models\Product;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Illuminate\Support\Facades\Cache;
 
 class ProductController extends Controller
 {
-    public function index(): Collection{
-        return Cache::remember('products', now()->addMinutes(10) , fn() => Product::without(['tags', 'content', 'stocks'])->get());
+    public function index(): AnonymousResourceCollection{
+        return Cache::remember('products', now()->addMinutes(10) ,
+            fn() =>
+            ProductsResource::collection(Product::without(['tags', 'content', 'stocks'])->get())
+        );
     }
 
     public function show(string $sku): Product | string {
---
+++ app/Http/Resources/ProductsResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductsResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'SKU' => $this->SKU,
+            'photo_url' => $this->photo_url
+        ];
+    }
+}
