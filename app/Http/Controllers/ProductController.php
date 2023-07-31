3ee07f17a4b12bbe1215a73c82037cc830f42051
Kipras Bielinskas <kipras.bielinskas@kantar.com>
2023-07-31 15:33:15 +0300

    Caching products on ProductController::index without any relations.

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Cache;
 
 class ProductController extends Controller
 {
     public function index(): Collection{
-        return Product::with(['content', 'tags', 'stocks'])->get();
+        return Cache::remember('products', now()->addMinutes(10) , fn() => Product::without(['tags', 'content', 'stocks'])->get());
     }
 
     public function show(string $sku): Product | string {
