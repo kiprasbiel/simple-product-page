b58c077035a9ce606e10b0583f36b3afac1438df
Kipras Bielinskas <kipras.bielinskas@kantar.com>
2023-07-31 16:24:12 +0300

    Caching single product API response. Configured resources for Product model and its relations.

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\ProductsResource;
+use App\Http\Resources\SingleProductResource;
 use App\Models\Product;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
@@ -11,14 +12,17 @@ use Illuminate\Support\Facades\Cache;
 class ProductController extends Controller
 {
     public function index(): AnonymousResourceCollection{
-        return Cache::remember('products', now()->addMinutes(10) ,
+        return Cache::remember('products', now()->addMinutes(10),
             fn() =>
             ProductsResource::collection(Product::without(['tags', 'content', 'stocks'])->get())
         );
     }
 
-    public function show(string $sku): Product | string {
-        return Product::where('SKU', $sku)->first() ?: '{}';
+    public function show(string $sku) {
+        return Cache::remember("SKU_$sku", now()->addMinutes(10),
+            fn() =>
+            new SingleProductResource(Product::where('SKU', $sku)->first())
+        );
     }
 
     public function similarProducts(Product $product): Collection {
