b57f4a60605339f4074b03e21c896ffda24bf18d
Kipras Bielinskas <kipras.bielinskas@kantar.com>
2023-08-01 18:14:44 +0300

    Updated ProductsResource by adding additional 'id' and 'size' fields.

--- app/Http/Resources/ProductsResource.php
+++ app/Http/Resources/ProductsResource.php
@@ -15,8 +15,10 @@ class ProductsResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'SKU' => $this->SKU,
-            'photo_url' => $this->photo_url
+            'photo_url' => $this->photo_url,
+            'size' => $this->size,
         ];
     }
 }
